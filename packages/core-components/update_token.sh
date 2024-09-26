9412cd58e3086eae20ce3a2f4a3168646ee4e4e7
woolf1717 <Barstpiczynski@gmail.com>
2024-09-26 14:54:20 +0200

    f

--- packages/core-components/update_token.sh
+++ packages/core-components/update_token.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-new_token=$(aws codeartifact get-authorization-token --domain woolf1717 --domain-owner 021891611839 --query authorizationToken --output text)
+new_token=$(aws codeartifact get-authorization-token --domain core-components --domain-owner 021891611839 --query authorizationToken --output text)
 
 sed -i "s|authToken=.*|authToken=$new_token|" .npmrc
 
